45e8db5e2455feae58f288ad41f2db39c4d8fb25
yunxiang <yunxiangwang777@gmail.com>
2025-09-18 23:34:55 +0800

    add: CLR機制

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,25 +2,14 @@
 
 class MyClass
 {
-    // 四個 int 欄位
-    public int a;
-    public int b;
-    public int c;
-    public int d;
+    // 宣告私有欄位
+    private int number;
 
-    // 建構子，用來初始化欄位
-    public MyClass(int a, int b, int c, int d)
+    // 公開屬性（property）提供 get 與 set 方法
+    public int Number
     {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-    }
-
-    // 方法：計算四個數字的總和
-    public int Sum()
-    {
-        return a + b + c + d;
+        get { return number; }      // 讀取值
+        set { number = value; }     // 設定值
     }
 }
 
@@ -28,14 +17,12 @@ class Program
 {
     static void Main()
     {
-        // 建立類別物件
-        MyClass obj = new MyClass(1, 2, 3, 4);
+        MyClass obj = new MyClass();
 
-        // 使用類別的方法
-        Console.WriteLine("Sum = " + obj.Sum());  // 輸出 Sum = 10
+        // 使用 set 設定值
+        obj.Number = 42;
 
-        // 直接修改欄位
-        obj.a = 10;
-        Console.WriteLine("New Sum = " + obj.Sum()); // 輸出 New Sum = 19
+        // 使用 get 取得值
+        Console.WriteLine(obj.Number);  // 輸出 42
     }
 }
