343a8e66bca302548c4664570b394ef54bd46f08
yunxiang <yunxiangwang777@gmail.com>
2025-09-18 19:22:22 +0800

    陣列: 參考型別

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,19 +4,16 @@ class Program
 {
     static void Main()
     {
-        int a = 10;
-        int b = 20;
-        int max = Getmx(a, b);
-        Console.WriteLine("Maximum value is: " + max);
+        int[] arr = { 1, 2, 3, 4, 5 };
+        upateArray(arr);
+        Console.WriteLine(string.Join(", ", arr));
     }
 
-    static int Getmx(int a, int b)
+    static void upateArray(int[] arr)
     {
-        int max = a;
-        if (b > max)
+        for (int i = 0; i < arr.Length; i++)
         {
-            max = b;
+            arr[i] += 5;
         }
-        return max;
     }
 }
