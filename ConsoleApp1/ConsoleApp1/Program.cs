ed5a4e29d742e28496041da59264744bc8d6cbfa
yunxiang <yunxiangwang777@gmail.com>
2025-09-18 19:15:36 +0800

    add: stack and heap

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,28 +4,19 @@ class Program
 {
     static void Main()
     {
-        int[] arr = { 22, 43, 12, 45, 32, 98, 54, 74, 51 };
+        int a = 10;
+        int b = 20;
+        int max = Getmx(a, b);
+        Console.WriteLine("Maximum value is: " + max);
+    }
 
-        for (int i = 1; i < arr.Length; i++)
+    static int Getmx(int a, int b)
+    {
+        int max = a;
+        if (b > max)
         {
-            int key = arr[i];      // 目前要插入的元素
-            int j = i - 1;
-
-            // 往前掃描已排序的子陣列，將比 key 大的元素往後移
-            while (j >= 0 && arr[j] > key)
-            {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-
-            // 將 key 插入正確位置
-            arr[j + 1] = key;
+            max = b;
         }
-
-        // 印出排序結果
-        foreach (int num in arr)
-        {
-            Console.Write(num + " ");
-        }
+        return max;
     }
 }
