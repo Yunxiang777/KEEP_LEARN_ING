5cd8419a6fb9c09cdd2ce0b99fd74d482377336b
yunxiang <yunxiangwang777@gmail.com>
2025-09-18 22:22:29 +0800

    add: 類的作用域衝突

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 
+class MyClass
+{
+    // 四個 int 欄位
+    public int a;
+    public int b;
+    public int c;
+    public int d;
+
+    // 建構子，用來初始化欄位
+    public MyClass(int a, int b, int c, int d)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    // 方法：計算四個數字的總和
+    public int Sum()
+    {
+        return a + b + c + d;
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        int[] arr = { 1, 2, 3, 4, 5 };
-        upateArray(arr);
-        Console.WriteLine(string.Join(", ", arr));
-    }
+        // 建立類別物件
+        MyClass obj = new MyClass(1, 2, 3, 4);
 
-    static void upateArray(int[] arr)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] += 5;
-        }
+        // 使用類別的方法
+        Console.WriteLine("Sum = " + obj.Sum());  // 輸出 Sum = 10
+
+        // 直接修改欄位
+        obj.a = 10;
+        Console.WriteLine("New Sum = " + obj.Sum()); // 輸出 New Sum = 19
     }
 }
