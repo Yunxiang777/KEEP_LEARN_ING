cdf7678e092dd0c2a2198113fc0843b520e157b6
yunxiang <yunxiangwang777@gmail.com>
2025-09-20 00:59:30 +0800

    add: c#/重載運算符

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,24 +4,46 @@ class Program
 {
     static void Main()
     {
-        string myString = "hello world";
+        Point p1 = new Point(3, 5);
+        Point p2 = new Point(2, 7);
 
-        // 使用擴充方法
-        string result = myString.FirstUpper();
+        // 使用重載的 + 運算符
+        Point sum = p1 + p2;
+        Console.WriteLine(sum); // 輸出: (5, 12)
 
-        Console.WriteLine(result); // 輸出: Hello world
+        // 使用重載的 - 運算符
+        Point diff = p1 - p2;
+        Console.WriteLine(diff); // 輸出: (1, -2)
     }
 }
 
-// 靜態類別放擴充方法
-public static class Tool
+// 定義 Point 類別
+public class Point
 {
-    // 擴充方法語法：this string input
-    public static string FirstUpper(this string input)
-    {
-        if (string.IsNullOrEmpty(input))
-            return input;
+    public int X { get; set; }
+    public int Y { get; set; }
 
-        return char.ToUpper(input[0]) + input.Substring(1);
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // 重載 + 運算符
+    public static Point operator +(Point a, Point b)
+    {
+        return new Point(a.X + b.X, a.Y + b.Y);
+    }
+
+    // 重載 - 運算符
+    public static Point operator -(Point a, Point b)
+    {
+        return new Point(a.X - b.X, a.Y - b.Y);
+    }
+
+    // 重載 ToString() 方便輸出
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
     }
 }
