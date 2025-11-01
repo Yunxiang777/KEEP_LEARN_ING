b155252f759b21f672be6a352dfecc6ca5cee1a2
yunxiang <yunxiangwang777@gmail.com>
2025-11-01 21:42:12 +0800

    add: nodejs

--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
-const fs = require('fs');
-fs.writeFileSync('/hello.txt', 'Hello World');
+const path = require('path');
+console.log('Hello, World!');
+console.log('Current file path:', path.resolve(__filename, '/index.html'));
