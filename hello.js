d0ff6cf3277008f4cc602084b8f75042b7436f4b
yunxiang <yunxiangwang777@gmail.com>
2025-11-03 20:34:27 +0800

    add: nodejs

--- hello.js
+++ hello.js
@@ -1,10 +1,2 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  
-  res.end('Hello, World!\n');
-});
-
-server.listen(9000, () => {
-  console.log('Server is listening on port 3000');
-});
+const tiemo = require('./me');
+tiemo.tiemo();
